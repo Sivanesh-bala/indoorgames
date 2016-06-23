cc148d536fd1909fefc98231afae6606eefd0770
Sivanesh-bala <sivaneshbala22@gmail.com>
2016-06-23 17:20:30 +0530

    23.06.2016

---
+++ GamesBackEnd/src/main/java/com/niit/GamesBackEnd/dao/SupplierDAO.java
@@ -0,0 +1,19 @@
+package com.niit.GamesBackEnd.dao;
+
+import java.util.List;
+
+import com.niit.GamesBackEnd.model.Supplier;
+
+public interface SupplierDAO {
+	
+	public List<Supplier> list();
+
+	public Supplier get(String id);
+
+	public void saveOrUpdate(Supplier supplier);
+
+	public void delete(String id);
+
+
+
+}
