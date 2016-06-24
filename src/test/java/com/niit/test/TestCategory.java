529bb2d6d3ee9adfc5adda1cfdac7ff47eea7f3b
Sivanesh-bala <sivaneshbala22@gmail.com>
2016-06-24 16:30:26 +0530

    24.06.2016

---
+++ src/test/java/com/niit/test/TestCategory.java
@@ -0,0 +1,29 @@
+package com.niit.test;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.niit.GamesBackEnd.dao.CategoryDAO;
+import com.niit.GamesBackEnd.model.Category;
+
+public class TestCategory {
+	
+	public static void main(String[] args)
+	{
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		context.scan("com.niit.GamesBackEnd");
+		context.refresh();
+				CategoryDAO categoryDAO = (CategoryDAO) context.getBean("categoryDAO");
+		
+		Category category = new Category();
+		category.setId("1");
+		category.setName("Mobile");
+		category.setDescription("This is Mobile");
+		
+		categoryDAO.saveOrUpdate(category);
+		
+		
+		categoryDAO.delete("1");
+		
+		
+}
+}
